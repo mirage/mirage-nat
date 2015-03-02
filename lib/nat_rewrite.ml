c8c82ead1069767bab3ad5a96891a873bf5e6f7f
Mindy Preston <meetup@yomimono.org>
2015-03-02 17:14:48 +0000

    Phantom typing for packet decomposing functions.
    Provide phantom types `ethernet layer`, `ip layer`, and `transport
    layer` for `Cstruct.t`, accessible by calling `Nat_rewrite.layers`.  The returned
    tuple of `Cstruct.t`s should be usable ethernet, IP, and transport
    layers respectively, on which functions like `addresses_of_ip` can be
    called.  Length of the `Cstruct.t`s is not truncated at the
    layer boundary, so the `Cstruct`s returned from `layers` have overlapping views into the same
    buffers.
    
    This change was made to allow `mirage-nat` and its callers to execute
    fewer repeated checks on `Cstruct.t` structure, and prevent mistakes
    from confusing `Cstruct.t` variables representing different data.

--- lib/nat_rewrite.ml
+++ lib/nat_rewrite.ml
@@ -77,6 +77,11 @@ type insert_result =
   | Overlap
   | Unparseable
 
+type 'a layer = Cstruct.t
+type ethernet
+type ip
+type transport
+
 (* reproduced from ipv4.checksum *)
 let checksum =
   let pbuf = Cstruct.create 4 in
@@ -90,19 +95,16 @@ let checksum =
 
 (* TODO: it's not clear where this function should be, but it probably shouldn't
    be here in the long run. *)
-let retrieve_ips frame =
-  let ip_type = Wire_structs.get_ethernet_ethertype frame in
-  let ip_packet = Cstruct.shift frame Wire_structs.sizeof_ethernet in
+let addresses_of_ip ip_packet =
+  let hlen_version = Wire_structs.get_ipv4_hlen_version ip_packet in
+  let ip_type = ((hlen_version land 0xf0) lsr 4) in
   match ip_type with
-  | 0x0800 -> (* ipv4 *)
-    Some
+  | 4 -> (* ipv4 *)
     (Ipaddr.V4 (Ipaddr.V4.of_int32 (Wire_structs.get_ipv4_src ip_packet)),
      Ipaddr.V4 (Ipaddr.V4.of_int32 (Wire_structs.get_ipv4_dst ip_packet)))
-  | 0x86dd -> (* ipv6 *)
-    Some
+  | 6 -> (* ipv6 *)
     (Ipaddr.V6 (V6.of_cstruct_exn (Wire_structs.Ipv6_wire.get_ipv6_src ip_packet)),
      Ipaddr.V6 (V6.of_cstruct_exn (Wire_structs.Ipv6_wire.get_ipv6_dst ip_packet)))
-  | _ -> None
 
 let retrieve_ports tx_layer =
   (* Cstruct.uint16, Cstruct.uint16 *)
@@ -124,45 +126,38 @@ let ip_and_above_of_frame frame =
     else Some (Cstruct.shift frame Wire_structs.sizeof_ethernet)
   | _ -> None
 
-let transport_and_above_of_ip ip =
-  let hlen_version = Wire_structs.get_ipv4_hlen_version ip in
+let proto_of_ip ip_layer =
+  let hlen_version = Wire_structs.get_ipv4_hlen_version ip_layer in
   match ((hlen_version land 0xf0) lsr 4) with
-  | 4 -> (* length (in words, not bytes) is in the other half of hlen_version *)
-    Some (Cstruct.shift ip ((hlen_version land 0x0f) * 4))
-  | 6 -> (* ipv6 is a constant length *)
-    Some ( Cstruct.shift ip Wire_structs.Ipv6_wire.sizeof_ipv6)
-  | n -> None
+  | 4 -> Wire_structs.get_ipv4_proto ip_layer
+  | 6 -> Wire_structs.Ipv6_wire.get_ipv6_nhdr ip_layer
 
-let proto_of_frame frame =
-  match ip_and_above_of_frame frame with
+let transport_and_above_of_ip ip =
+  let long_enough = function
+    | 6 -> Wire_structs.Tcp_wire.sizeof_tcp
+    | 17 -> Wire_structs.sizeof_udp
+  in
+  let length hlen_version = 
+    match ((hlen_version land 0xf0) lsr 4) with
+    | 4 -> (* length (in words, not bytes) is in the other half of hlen_version *)
+      Some ((hlen_version land 0x0f) * 4)
+    | 6 -> (* ipv6 is a constant length *)
+      Some Wire_structs.Ipv6_wire.sizeof_ipv6
+    | n -> None
+  in
+  let hlen_version = Wire_structs.get_ipv4_hlen_version ip in
+  match length hlen_version with
   | None -> None
-  | Some ip_layer -> Some (Wire_structs.get_ipv4_proto ip_layer)
+  | Some n -> 
+    let minimum_tx_header = long_enough (proto_of_ip ip) in
+    if ((Cstruct.len ip) < (n + minimum_tx_header)) then
+      None 
+    else
+      Some (Cstruct.shift ip n)
 
-let ips_of_frame frame =
-  let ip_type = Wire_structs.get_ethernet_ethertype frame in
-  let ip_packet = Cstruct.shift frame Wire_structs.sizeof_ethernet in
-  match ip_type with
-  | 0x0800 -> (* ipv4 *)
-    if (Cstruct.len ip_packet) < (Wire_structs.sizeof_ipv4) then None else
-    Some
-    (Ipaddr.V4 (Ipaddr.V4.of_int32 (Wire_structs.get_ipv4_src ip_packet)),
-     Ipaddr.V4 (Ipaddr.V4.of_int32 (Wire_structs.get_ipv4_dst ip_packet)))
-  | 0x86dd -> (* ipv6 *)
-    Some
-    (Ipaddr.V6 (V6.of_cstruct_exn (Wire_structs.Ipv6_wire.get_ipv6_src ip_packet)),
-     Ipaddr.V6 (V6.of_cstruct_exn (Wire_structs.Ipv6_wire.get_ipv6_dst ip_packet)))
-  | _ -> None
-
-let ports_of_frame frame =
-  match ip_and_above_of_frame frame with
-  | None -> None
-  | Some ip_layer ->
-    match transport_and_above_of_ip ip_layer with
-    | None -> None
-    | Some tx_layer ->
-      if (Cstruct.len tx_layer < (Wire_structs.sizeof_udp)) then None else Some
-          ((Wire_structs.get_udp_source_port tx_layer : int),
-           (Wire_structs.get_udp_dest_port tx_layer : int))
+let ports_of_transport tx_layer =
+  ((Wire_structs.get_udp_source_port tx_layer : int),
+   (Wire_structs.get_udp_dest_port tx_layer : int))
 
 let layers frame =
   let bind f x y =
@@ -194,10 +189,6 @@ let rewrite_port (txlayer : Cstruct.t) direction (sport, dport) =
 let translate table direction frame =
   (* note that ethif.input doesn't have the same register-listeners-then-input
      format that tcp/udp do, so we could use it for the outer layer of parsing *)
-  let ip_size is_ipv6 = match is_ipv6 with
-    | false -> Wire_structs.sizeof_ipv4
-    | true -> Wire_structs.Ipv6_wire.sizeof_ipv6
-  in
   let decrement_ttl ip_layer =
     Wire_structs.set_ipv4_ttl ip_layer
       ((Wire_structs.get_ipv4_ttl ip_layer) - 1)
@@ -212,94 +203,84 @@ let translate table direction frame =
     let new_csum = Tcpip_checksum.ones_complement just_ipv4 in
     Wire_structs.set_ipv4_csum ip_layer new_csum
   in
-  let ip_packet = Cstruct.shift frame Wire_structs.sizeof_ethernet in
-  match (retrieve_ips frame) with
-  | Some (V4 src, V4 dst) -> (* ipv4 *) (
-      let proto = Wire_structs.get_ipv4_proto ip_packet in
-      match proto with
-      | 6 | 17 -> (
-          let higherproto_packet = Cstruct.shift ip_packet (ip_size false) in
-          match retrieve_ports higherproto_packet with
-          | Some (sport, dport) -> (
+  match (layers frame) with
+  | None -> None (* un-NATtable packet; drop it like it's hot *)
+  | Some (frame, ip_packet, higherproto_packet) ->
+    match (addresses_of_ip ip_packet) with
+    | (V4 src, V4 dst) -> (* ipv4 *) (
+        let (proto : int) = proto_of_ip ip_packet in
+        match proto with
+        | 6 | 17 -> (
+            let (sport, dport) = ports_of_transport higherproto_packet in
             (* got everything; do the lookup *)
             let result = Nat_lookup.lookup table proto ((V4 src), sport) ((V4 dst), dport)
             in
             match result with
-              | Some ((V4 new_src, new_sport), (V4 new_dst, new_dport)) ->
-                (* TODO: we should probably refuse to pass TTL = 0 and instead send an
-                   ICMP message back to the sender *)
-                rewrite_ip false ip_packet direction (V4 new_src, V4 new_dst);
-                rewrite_port higherproto_packet direction (new_sport, new_dport);
-                decrement_ttl ip_packet;
-                recalculate_ip_checksum ip_packet;
-                Some frame
+            | None -> None (* don't autocreate new entries *)
+            | Some ((V4 new_src, new_sport), (V4 new_dst, new_dport)) ->
+              (* TODO: we should probably refuse to pass TTL = 0 and instead send an
+                 ICMP message back to the sender *)
+              rewrite_ip false ip_packet direction (V4 new_src, V4 new_dst);
+              rewrite_port higherproto_packet direction (new_sport, new_dport);
+              decrement_ttl ip_packet;
+              recalculate_ip_checksum ip_packet;
+              Some frame
 
-              (* TODO: 4-to-6 logic *)
-              | Some ((V6 new_src, new_sport), (V6 new_dst, new_dport)) -> None
-              | Some ((V6 _, _), (V4 _, _)) ->
-                raise (Invalid_argument "Impossible transformation in NAT
+            (* TODO: 4-to-6 logic *)
+            | Some ((V6 new_src, new_sport), (V6 new_dst, new_dport)) -> None
+            | Some ((V6 _, _), (V4 _, _)) ->
+              raise (Invalid_argument "Impossible transformation in NAT
                                          table (src ipv6, dst ipv4)")
-              | Some ((V4 _, _), (V6 _, _)) ->
-                raise (Invalid_argument "Impossible transformation in NAT
+            | Some ((V4 _, _), (V6 _, _)) ->
+              raise (Invalid_argument "Impossible transformation in NAT
                                          table (src ipv4, dst ipv6)")
-              | None -> None (* don't autocreate new entries *)
-            )
-          | None -> None (* udp/tcp but couldn't get ports; drop it *)
-        )
-      | _ -> None (* TODO: don't just drop all non-udp, non-tcp packets *)
-    )
-  | Some (V6 src, V6 dst) -> None (* TODO, obviously *) (* ipv6 *)
-  | _ -> None (* don't forward arp or other types *)
+          )
+        | _ -> None (* TODO: don't just drop all non-udp, non-tcp packets *)
+      )
+    | (V6 src, V6 dst) -> None (* TODO, obviously *) (* ipv6 *)
 
 let make_entry mode table frame
     (other_xl_ip, other_xl_port)
     (final_destination_ip, final_destination_port) =
-  (* basic sanity check; nothing smaller than this will be nat-able *)
-  if (Cstruct.len frame) < (Wire_structs.sizeof_ethernet +
-                          Wire_structs.sizeof_ipv4 + Wire_structs.sizeof_udp)
-  then
-    Unparseable
-  else
-    let ip_layer = Cstruct.shift frame (Wire_structs.sizeof_ethernet) in
-    let tx_layer = Cstruct.shift ip_layer (Wire_structs.sizeof_ipv4) in
-    let proto = Wire_structs.get_ipv4_proto ip_layer in
+  (* decompose this frame; if we can't, bail out now *)
+  match layers frame with
+  | None -> Unparseable
+  | Some (frame, ip_layer, tx_layer) ->
+    let proto = proto_of_ip ip_layer in
     let check_scope ip =
       match Ipaddr.scope ip with
       | Global | Organization -> true
       | _ -> false
     in
-    match (retrieve_ips frame), (retrieve_ports tx_layer) with
-    | Some (frame_src_ip, frame_dst_ip),
-      Some (frame_sport, frame_dport) -> begin
-        (* only Organization and Global scope IPs get routed *)
-        match check_scope frame_src_ip, check_scope frame_dst_ip with
-        | true, true -> (
-            let t =
-              match (mode : Nat_lookup.mode) with
-              | Nat ->
-                Nat_lookup.insert ~mode:Nat table proto
-                        (frame_src_ip, frame_sport) (frame_dst_ip, frame_dport)
-                        (other_xl_ip, other_xl_port) (other_xl_ip, other_xl_port)
-              | Redirect ->
-                (* in redirect mode, frame_src_ip and frame_dst_ip aren't what we're expecting --
-                the packet actually addressed to one of the xl ip/port pairs,
-                  and the next hop is sent to us in the arguments for this
-                   function. *)
+    let (frame_src_ip, frame_dst_ip) = addresses_of_ip ip_layer in
+    let (frame_sport, frame_dport) = ports_of_transport tx_layer in
+    (* only Organization and Global scope IPs get routed *)
+    match check_scope frame_src_ip, check_scope frame_dst_ip with
+    | true, true -> (
+        let t =
+          match (mode : Nat_lookup.mode) with
+          | Nat ->
+            Nat_lookup.insert ~mode:Nat table proto
+              (frame_src_ip, frame_sport) (frame_dst_ip, frame_dport)
+              (other_xl_ip, other_xl_port) (other_xl_ip, other_xl_port)
+          | Redirect ->
+            (* in redirect mode, frame_src_ip and frame_dst_ip aren't what we're expecting --
+               the packet actually addressed to one of the xl ip/port pairs,
+               and the next hop is sent to us in the arguments for this
+               function. *)
 
-                (* left side, right side, internal_xl, external_xl *)
-                  Nat_lookup.insert ~mode:Redirect table proto
-                    (frame_src_ip, frame_sport)
-                    (final_destination_ip, final_destination_port)
-                    (frame_dst_ip, frame_dport)
-                    (other_xl_ip, other_xl_port)
+            (* left side, right side, internal_xl, external_xl *)
+            Nat_lookup.insert ~mode:Redirect table proto
+              (frame_src_ip, frame_sport)
+              (final_destination_ip, final_destination_port)
+              (frame_dst_ip, frame_dport)
+              (other_xl_ip, other_xl_port)
 
-            in
-            match t with
-            | Some t -> Ok t
-            | None -> Overlap
-          )
-        | _, _ -> Unparseable
-      end
+        in
+        match t with
+        | Some t -> Ok t
+        | None -> Overlap
+      )
     | _, _ -> Unparseable
 
 (* the frame is addressed to one of our IPs.  We should rewrite the source with
--- lib/nat_rewrite.mli
+++ lib/nat_rewrite.mli
@@ -7,11 +7,16 @@ type insert_result =
   | Overlap
   | Unparseable
 
+(* phantom types for Cstructs, so type system can help us keep them straight *)
+type transport
+type ethernet
+type ip
+type 'a layer
+
 (** given a lookup table, rewrite direction, and an ip-level frame,
   * perform any translation indicated by presence in the table
   * on the Cstruct.t .  If the packet should be forwarded, return Some packet,
-  * else return None.  (TODO: this doesn't really make sense in the context of a
-  * library function; separate out this logic.)
+  * else return None.  
   * This function is zero-copy and mutates values in the given Cstruct.  *)
 val translate : Nat_lookup.t -> direction -> Cstruct.t -> Cstruct.t option
 
@@ -27,10 +32,10 @@ val translate : Nat_lookup.t -> direction -> Cstruct.t -> Cstruct.t option
   * otherwise, return an error type indicating the problem. *)
 val make_nat_entry : Nat_lookup.t -> Cstruct.t -> Ipaddr.t -> int -> insert_result
 
-(** given a table, a frame, and a translation IP and port, 
-  * put relevant entries for the (src_ip, src_port), (dst_ip, dst_port) from the
-  * frame (xl_ip, xl_port) (xl_right_port), depending on the mode argument.
-    entries will look like:
+(** given a table, a frame from which (src_ip, src_port) and (xl_left_ip,
+    xl_left_port) can be extracted (these are source and destination for the
+    packet), a translation (xl_left_ip, xl_left_port) pair, and a final
+    destination (dst_ip, dst_port) pair, add entries to table of the form:
     ((src_ip, src_port), (xl_left_ip, xl_left_port)) to 
          ((xl_right_ip, xl_right_port), (dst_ip, dst_port)) and
     ((dst_ip, dst_port), (xl_right_ip, xl_right_port)) to 
@@ -41,23 +46,23 @@ val make_nat_entry : Nat_lookup.t -> Cstruct.t -> Ipaddr.t -> int -> insert_resu
 val make_redirect_entry : Nat_lookup.t -> Cstruct.t -> (Ipaddr.t * int) 
   -> (Ipaddr.t * int) -> insert_result
 
-(* given an ethernet-and-above frame, fish out the src and dst ip *)
-val ips_of_frame : Cstruct.t -> (Ipaddr.t * Ipaddr.t) option
+(* given an ip packet, fish out the src and dst ip *)
+val addresses_of_ip : ip layer -> (Ipaddr.t * Ipaddr.t)
 
-(* given an ethernet-and-above frame, fish out the transport-layer protocol *)
-val proto_of_frame : Cstruct.t -> int option
+(* given an ip packet, fish out the transport-layer protocol number *)
+val proto_of_ip : ip layer -> int
 
-(* given an ethernet-and-above frame, fish out the transport-layer source and
+(* given a transport-layer packet, fish out the transport-layer source and
    destination ports *)
-val ports_of_frame : Cstruct.t -> (int * int) option
+val ports_of_transport : transport layer -> (int * int)
 
 (* attempt to decompose a frame into Cstructs representing the ethernet, ip, and
   tx layers.  each cstruct maintains a view into those above it (i.e., the
    ethernet cstruct's length is not set to the length of the ethernet header).
 *)
-val layers : Cstruct.t -> (Cstruct.t * Cstruct.t * Cstruct.t) option
+val layers : Cstruct.t -> (ethernet layer * ip layer * transport layer) option
 
 (* support for direct rewriting of packets *)
-val rewrite_ip : bool -> Cstruct.t -> direction -> (Ipaddr.t * Ipaddr.t) -> unit
+val rewrite_ip : bool -> ip layer -> direction -> (Ipaddr.t * Ipaddr.t) -> unit
 
-val rewrite_port : Cstruct.t -> direction -> (int * int) -> unit
+val rewrite_port : transport layer -> direction -> (int * int) -> unit
