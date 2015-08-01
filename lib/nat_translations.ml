1bfcb96d0115981eb5dd3be883d94e5a3afa63d9
Mindy Preston <meetup@yomimono.org>
2015-08-01 17:45:56 +0100

    include nat_translations (nat and redirect)

---
+++ lib/nat_translations.ml
@@ -0,0 +1,20 @@
+type endpoint = Nat_lookup.endpoint
+type mapping = Nat_lookup.mapping
+type result = {internal_lookup: mapping;
+               external_lookup: mapping;
+               internal_mapping: mapping;
+               external_mapping: mapping }
+
+let map_nat ~left ~right ~translate_left =
+  let internal_lookup = (left, right) in
+  let external_lookup = (right, translate_left) in
+  let internal_mapping = (translate_left, right) in
+  let external_mapping = (right, left) in
+  {internal_lookup; external_lookup; internal_mapping; external_mapping}
+
+let map_redirect ~left ~right ~translate_left ~translate_right =
+  let internal_lookup = (left, translate_left) in
+  let external_lookup = (right, translate_right) in
+  let internal_mapping = (translate_right, right) in
+  let external_mapping = (translate_left, left) in
+  {internal_lookup; external_lookup; internal_mapping; external_mapping}
