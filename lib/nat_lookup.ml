7f83e7c96d4d4a9b81f63bc7fe0800f2ed111441
Mindy Preston <meetup@yomimono.org>
2015-08-08 15:31:25 +0100

    introduce Nat_lookup.Make, parameterized by Irmin.S_MAKER

--- lib/nat_lookup.ml
+++ lib/nat_lookup.ml
@@ -1,6 +1,6 @@
 (* TODO: what are the data types on protocol numbers?  no explicit
-types in tcpip/lib/ipv4.ml, just matches on the number
-straight from the struct, so we'll do that too although we
+   types in tcpip/lib/ipv4.ml, just matches on the number
+   straight from the struct, so we'll do that too although we
    should instead restrict to tcp or udp *)
 
 (* TODO: types should be more complex and allow for entries mapping
@@ -25,81 +25,103 @@ let node proto = match proto with
 
 let (>>=) = Lwt.bind
 
-module T = Inds_table.Make(Nat_table.Key)(Nat_table.Entry)(Irmin.Path.String_list)
-module Backend = Irmin_mem.Make
-module I = Irmin.Basic (Backend)(T)
-type t = (string -> I.t)
+module type S = sig
+  type t 
 
-let expired _ = false (* stub *)
-let owner = "natbot"
-let config = Irmin_mem.config () (* both config & task need to be parameterized *)
-let task = Irmin.Task.create ~date:0L ~owner
-let empty () =
-  I.create config task >>= fun table ->
-  I.update (table "TCP table initialization") (node Tcp) (T.empty) >>= fun () ->
-  I.update (table "UDP table initialization") (node Udp) (T.empty) >>= fun () ->
-  Lwt.return table
+  val lookup : t -> protocol -> source:endpoint -> destination:endpoint ->
+    (endpoint * endpoint) option Lwt.t
+
+  val insert : t -> float -> protocol ->
+    internal_lookup:mapping -> 
+    external_lookup:mapping ->
+    internal_mapping:mapping ->
+    external_mapping:mapping -> t option Lwt.t
+
+  val delete : t -> protocol ->
+    internal_lookup:mapping -> external_lookup:mapping -> t Lwt.t
+
+  val empty : unit -> t Lwt.t
+end
+
+module Make(Backend: Irmin.S_MAKER) = struct
+
+  module T = Inds_table.Make(Nat_table.Key)(Nat_table.Entry)(Irmin.Path.String_list)
+  module Backend = Irmin_mem.Make
+  module I = Irmin.Basic (Backend)(T)
+  type t = (string -> I.t)
+
+  let expired _ = false (* stub *)
+  let owner = "natbot"
+  let config = Irmin_mem.config () (* both config & task need to be parameterized *)
+  let task = Irmin.Task.create ~date:0L ~owner
+  let empty () =
+    I.create config task >>= fun table ->
+    I.update (table "TCP table initialization") (node Tcp) (T.empty) >>= fun () ->
+    I.update (table "UDP table initialization") (node Udp) (T.empty) >>= fun () ->
+    Lwt.return table
 
 
-(* TODO: this is extremely awkward; can we expose a more proper T.mem from T.M? *)
-let mem table key = T.M.mem key table
+  (* TODO: this is extremely awkward; can we expose a more proper T.mem from T.M? *)
+  let mem table key = T.M.mem key table
 
-let lookup table proto ~source ~destination =
-  try
-    I.read_exn (table "read for lookup") (node proto) >>= fun table ->
-    match (T.find (source, destination) table) with
-    | Nat_table.Entry.Confirmed (time, mapping) ->
-      if (expired time) then Lwt.return None else Lwt.return (Some mapping)
-  with
-  | Not_found -> Lwt.return None
+  let lookup table proto ~source ~destination =
+    try
+      I.read_exn (table "read for lookup") (node proto) >>= fun table ->
+      match (T.find (source, destination) table) with
+      | Nat_table.Entry.Confirmed (time, mapping) ->
+        if (expired time) then Lwt.return None else Lwt.return (Some mapping)
+    with
+    | Not_found -> Lwt.return None
 
-let in_branch table proto ~head ~read ~update ~merge fn =
-  I.head_exn (table head) >>= fun head ->
-  I.of_head config task head >>= fun branch ->
-  I.read (branch read) (node proto) >>= function
-  | None -> Lwt.return None
-  | Some map ->
-    let map = T.to_map map in
-    match (fn map) with
+  let in_branch table proto ~head ~read ~update ~merge fn =
+    I.head_exn (table head) >>= fun head ->
+    I.of_head config task head >>= fun branch ->
+    I.read (branch read) (node proto) >>= function
     | None -> Lwt.return None
     | Some map ->
-      I.update (branch update) (node proto) (T.of_map map) >>= fun () ->
-      I.merge_exn merge branch ~into:table >>= fun () -> Lwt.return (Some table)
+      let map = T.to_map map in
+      match (fn map) with
+      | None -> Lwt.return None
+      | Some map ->
+        I.update (branch update) (node proto) (T.of_map map) >>= fun () ->
+        I.merge_exn merge branch ~into:table >>= fun () -> Lwt.return (Some table)
 
-(* cases that should result in a valid mapping:
-   neither side is already mapped *)
-let insert table expiration proto
-    ~internal_lookup ~external_lookup ~internal_mapping ~external_mapping =
-  let insertor (map : Nat_table.Entry.t T.M.t) = 
-    let check proto (src, dst) = mem map (src, dst) in
-    match (check proto internal_lookup, check proto external_lookup) with
-    | false, false ->
-      let map = T.M.add internal_lookup
-          (Nat_table.Entry.Confirmed (expiration, internal_mapping)) map in
-      let map = T.M.add external_lookup
-          (Nat_table.Entry.Confirmed (expiration, external_mapping)) map in
+  (* cases that should result in a valid mapping:
+     neither side is already mapped *)
+  let insert table expiration proto
+      ~internal_lookup ~external_lookup ~internal_mapping ~external_mapping =
+    let insertor (map : Nat_table.Entry.t T.M.t) = 
+      let check proto (src, dst) = mem map (src, dst) in
+      match (check proto internal_lookup, check proto external_lookup) with
+      | false, false ->
+        let map = T.M.add internal_lookup
+            (Nat_table.Entry.Confirmed (expiration, internal_mapping)) map in
+        let map = T.M.add external_lookup
+            (Nat_table.Entry.Confirmed (expiration, external_mapping)) map in
+        Some map
+      | true, true (* currently the semantics enforced by the unit tests are to
+                         reject duplicate entries, although it may be necessary to
+                         change this in the future *)
+      | _, _ -> None
+    in
+    in_branch table proto
+      ~head:"branching to add entry"
+      ~read:"reading to add entry" 
+      ~update:"add entry"
+      ~merge:"merge after completing add entry"
+      insertor
+
+  let delete table proto ~internal_lookup ~external_lookup =
+    let remover map = 
+      let map = T.M.remove internal_lookup map in
+      let map = T.M.remove external_lookup map in
       Some map
-    | true, true (* currently the semantics enforced by the unit tests are to
-                       reject duplicate entries, although it may be necessary to
-                       change this in the future *)
-    | _, _ -> None
-  in
-  in_branch table proto
-    ~head:"branching to add entry"
-    ~read:"reading to add entry" 
-    ~update:"add entry"
-    ~merge:"merge after completing add entry"
-    insertor
+    in
+    in_branch table proto
+      ~head:"branching to remove entry"
+      ~read:"reading to remove entry"
+      ~update:"remove entry"
+      ~merge:"merge after completing remove entry"
+      remover >>= fun _ -> Lwt.return table
 
-let delete table proto ~internal_lookup ~external_lookup =
-  let remover map = 
-    let map = T.M.remove internal_lookup map in
-    let map = T.M.remove external_lookup map in
-    Some map
-  in
-  in_branch table proto
-    ~head:"branching to remove entry"
-    ~read:"reading to remove entry"
-    ~update:"remove entry"
-    ~merge:"merge after completing remove entry"
-    remover >>= fun _ -> Lwt.return table
+end
--- lib/nat_lookup.mli
+++ lib/nat_lookup.mli
@@ -2,22 +2,29 @@ type protocol = | Udp | Tcp
 type port = int
 type endpoint = Nat_table.Endpoint.t
 type mapping = (endpoint * endpoint)
-type t 
 
 type mode =
   | Redirect
   | Nat
 
-val lookup : t -> protocol -> source:endpoint -> destination:endpoint ->
-  (endpoint * endpoint) option Lwt.t
+module type S = sig
+  type t 
 
-val insert : t -> float -> protocol ->
-  internal_lookup:mapping -> 
-  external_lookup:mapping ->
-  internal_mapping:mapping ->
-  external_mapping:mapping -> t option Lwt.t
+  val lookup : t -> protocol -> source:endpoint -> destination:endpoint ->
+    (endpoint * endpoint) option Lwt.t
 
-val delete : t -> protocol ->
-  internal_lookup:mapping -> external_lookup:mapping -> t Lwt.t
+  val insert : t -> float -> protocol ->
+    internal_lookup:mapping -> 
+    external_lookup:mapping ->
+    internal_mapping:mapping ->
+    external_mapping:mapping -> t option Lwt.t
 
-val empty : unit -> t Lwt.t
+  val delete : t -> protocol ->
+    internal_lookup:mapping -> external_lookup:mapping -> t Lwt.t
+
+  val empty : unit -> t Lwt.t
+end
+
+module Make(I : Irmin.S_MAKER) : sig
+  include S
+end
