58da6bc1dfadb4f1e0086aca5082bda191535210
Mindy Preston <meetup@yomimono.org>
2015-08-08 15:32:15 +0100

    introduce Nat_rewrite.Make, parameterized over Nat_lookup.S

--- lib/nat_rewrite.mli
+++ lib/nat_rewrite.mli
@@ -2,50 +2,53 @@
    or the destination IP and port?  *)
 type direction = Source | Destination
 
-type insert_result =
-  | Ok of Nat_lookup.t
-  | Overlap
-  | Unparseable
+module Make(N: Nat_lookup.S) : sig
+  type insert_result =
+    | Ok of N.t
+    | Overlap
+    | Unparseable
 
-(** given a lookup table, rewrite direction, and an ip-level frame,
-  * perform any translation indicated by presence in the table
-  * on the Cstruct.t .  If the packet should be forwarded, return Some packet,
-  * else return None.  
-  * This function is zero-copy and mutates values in the given Cstruct.  *)
-val translate : Nat_lookup.t -> direction -> Cstruct.t -> Cstruct.t option Lwt.t
+  (** given a lookup table, rewrite direction, and an ip-level frame,
+    * perform any translation indicated by presence in the table
+    * on the Cstruct.t .  If the packet should be forwarded, return Some packet,
+    * else return None.  
+    * This function is zero-copy and mutates values in the given Cstruct.  *)
+  val translate : N.t -> direction -> Cstruct.t -> Cstruct.t option Lwt.t
 
-(** given a table, a frame, and a translation IP and port,
-  * put relevant entries for the (src_ip, src_port), (dst_ip, dst_port) from the
-  * frame and given (xl_ip, xl_port).
-    entries will look like:
-    ((src_ip, src_port), (dst_ip, dst_port) to
-       (xl_ip, xl_port), (dst_ip, dst_port)) and
-    ((dst_ip, dst_port), (xl_ip, xl_port)) to
-       (dst_ip, dst_port), (src_ip, src_port)).
-  * if insertion succeeded, return the new table;
-  * otherwise, return an error type indicating the problem. *)
-val make_nat_entry : Nat_lookup.t -> Cstruct.t -> Ipaddr.t -> int ->
-  insert_result Lwt.t
+  (** given a table, a frame, and a translation IP and port,
+    * put relevant entries for the (src_ip, src_port), (dst_ip, dst_port) from the
+    * frame and given (xl_ip, xl_port).
+      entries will look like:
+      ((src_ip, src_port), (dst_ip, dst_port) to
+         (xl_ip, xl_port), (dst_ip, dst_port)) and
+      ((dst_ip, dst_port), (xl_ip, xl_port)) to
+         (dst_ip, dst_port), (src_ip, src_port)).
+    * if insertion succeeded, return the new table;
+    * otherwise, return an error type indicating the problem. *)
+  val make_nat_entry : N.t -> Cstruct.t -> Ipaddr.t -> int ->
+    insert_result Lwt.t
 
-(** given a table, a frame from which (src_ip, src_port) and (xl_left_ip,
-    xl_left_port) can be extracted (these are source and destination for the
-    packet), a translation (xl_left_ip, xl_left_port) pair, and a final
-    destination (dst_ip, dst_port) pair, add entries to table of the form:
-    ((src_ip, src_port), (xl_left_ip, xl_left_port)) to 
-         ((xl_right_ip, xl_right_port), (dst_ip, dst_port)) and
-    ((dst_ip, dst_port), (xl_right_ip, xl_right_port)) to 
-         ((xl_left_ip, xl_left_port), (src_ip, src_port)).
-    ((xl_ip, xl_right_port), (dst_ip, dst_port)) to (src_ip, src_port).
-  * if insertion succeeded, return the new table;
-  * otherwise, return an error type indicating the problem. *)
-val make_redirect_entry : Nat_lookup.t -> Cstruct.t -> (Ipaddr.t * int) 
-  -> (Ipaddr.t * int) -> insert_result Lwt.t
+  (** given a table, a frame from which (src_ip, src_port) and (xl_left_ip,
+      xl_left_port) can be extracted (these are source and destination for the
+      packet), a translation (xl_left_ip, xl_left_port) pair, and a final
+      destination (dst_ip, dst_port) pair, add entries to table of the form:
+      ((src_ip, src_port), (xl_left_ip, xl_left_port)) to 
+           ((xl_right_ip, xl_right_port), (dst_ip, dst_port)) and
+      ((dst_ip, dst_port), (xl_right_ip, xl_right_port)) to 
+           ((xl_left_ip, xl_left_port), (src_ip, src_port)).
+      ((xl_ip, xl_right_port), (dst_ip, dst_port)) to (src_ip, src_port).
+    * if insertion succeeded, return the new table;
+    * otherwise, return an error type indicating the problem. *)
+  val make_redirect_entry : N.t -> Cstruct.t -> (Ipaddr.t * int) 
+    -> (Ipaddr.t * int) -> insert_result Lwt.t
+
+end
 
 (* phantom types for Cstructs, so type system can help us keep them straight *)
-type transport
-type ethernet
-type ip
-type 'a layer
+type transport = Nat_decompose.transport
+type ethernet = Nat_decompose.ethernet
+type ip = Nat_decompose.ip
+type 'a layer = 'a Nat_decompose.layer
 
 (* given a function for recalculating transport-layer checksums and an (ip,
    transport) pair, recalculate and set checksum for valid-looking udp or tcp
