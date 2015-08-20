0a0729388b64681332d8bd980bd6d0e8ef8de635
Mindy Preston <meetup@yomimono.org>
2015-08-20 14:05:41 +0100

    s/result/translation/

--- lib/nat_translations.mli
+++ lib/nat_translations.mli
@@ -1,13 +1,9 @@
-type endpoint = Nat_lookup.endpoint
-type mapping = Nat_lookup.mapping
-type result = {internal_lookup: mapping;
-               external_lookup: mapping;
-               internal_mapping: mapping;
-               external_mapping: mapping }
+open Nat_types
 
+(* needs better name -- kill map_ here *)
 val map_nat : left:endpoint -> right:endpoint -> translate_left:endpoint ->
-  result
+  translation
 
 val map_redirect : left:endpoint -> right:endpoint ->
   translate_left:endpoint -> translate_right:endpoint ->
-  result
+  translation
