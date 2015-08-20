96cb62a6b550e70bf6136cec4b99d18b00b2fe96
Mindy Preston <meetup@yomimono.org>
2015-08-20 14:06:01 +0100

    remove insert_mappings; insert is now appropriate for direct call

--- lib_test/test_lib.ml
+++ lib_test/test_lib.ml
@@ -6,11 +6,3 @@ module Unix_time = struct
 end
 
 module N = Nat_lookup.Make(Irmin_mem.Make)(Unix_clock)(Unix_time)
-
-let insert_mappings table expiry proto mappings =
-  let open Nat_translations in
-  N.insert table expiry proto
-    ~internal_lookup:mappings.internal_lookup
-    ~external_lookup:mappings.external_lookup
-    ~internal_mapping:mappings.internal_mapping
-    ~external_mapping:mappings.external_mapping
