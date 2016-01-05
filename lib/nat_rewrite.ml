a44cc5c230d384ab091f9c50ed55268e2b4bb87b
Mindy Preston <meetup@yomimono.org>
2016-01-05 15:24:32 +0000

    use packet mutation functions in nat_decompose

--- lib/nat_rewrite.ml
+++ lib/nat_rewrite.ml
@@ -7,43 +7,6 @@ type ethernet = Nat_decompose.ethernet
 type ip = Nat_decompose.ip
 type transport = Nat_decompose.transport
 
-let rewrite_ip is_ipv6 (ip_layer : Cstruct.t) direction i =
-  (* TODO: this is not the right set of parameters for a function that might
-       have to do 6-to-4 translation *)
-  (* also, TODO all of the 6-to-4/4-to-6 thoughts and code.  nbd. *)
-  match (is_ipv6, direction, i) with
-  | false, _, (V4 new_src, V4 new_dst) ->
-    Wire_structs.Ipv4_wire.set_ipv4_src ip_layer (Ipaddr.V4.to_int32 new_src);
-    Wire_structs.Ipv4_wire.set_ipv4_dst ip_layer (Ipaddr.V4.to_int32 new_dst)
-  | _, _, _ -> raise (Failure "ipv4-ipv4 is the only implemented case")
-
-let rewrite_port (txlayer : Cstruct.t) direction (sport, dport) =
-  Wire_structs.set_udp_source_port txlayer sport;
-  Wire_structs.set_udp_dest_port txlayer dport
-
-let recalculate_transport_checksum csum_fn (ethernet, ip_layer, transport_layer) =
-  match Nat_decompose.ethip_headers (ethernet, ip_layer) with
-  | None -> raise (Invalid_argument
-                     "Could not recalculate transport-layer checksum after NAT rewrite")
-  | Some just_headers ->
-    let fix_checksum set_checksum ip_layer higherlevel_data =
-      (* reset checksum to 0 for recalculation *)
-      set_checksum higherlevel_data 0;
-      let actual_checksum = csum_fn just_headers (higherlevel_data :: []) in
-      set_checksum higherlevel_data actual_checksum
-    in
-    let () = match Nat_decompose.proto_of_ip ip_layer with
-      | 17 -> fix_checksum Wire_structs.set_udp_checksum ip_layer transport_layer
-      | 6 ->
-        fix_checksum Wire_structs.Tcp_wire.set_tcp_checksum ip_layer transport_layer
-      | _ -> ()
-    in
-    (just_headers, transport_layer)
-
-let set_smac ethernet mac =
-  Wire_structs.set_ethernet_src (Macaddr.to_bytes mac) 0 ethernet;
-  ethernet
-
 module Make(Nat_table : Mirage_nat.Lookup) : sig
   include Mirage_nat.S with type config = Nat_table.config 
 end = struct
@@ -69,20 +32,10 @@ end = struct
     MProf.Trace.label "Nat_rewrite.translate";
     (* note that ethif.input doesn't have the same register-listeners-then-input
        format that tcp/udp do, so we could use it for the outer layer of parsing *)
-    let decrement_ttl ip_layer =
-      Wire_structs.Ipv4_wire.set_ipv4_ttl ip_layer
-        ((Wire_structs.Ipv4_wire.get_ipv4_ttl ip_layer) - 1)
-    in
     (* TODO: this is not correct for IPv6 *)
     (* TODO: it's not clear to me whether we need to do this, since most users
        will be sending packets via IP.write, which itself calculates and inserts
        the proper checksum before sending the packet. *)
-    let recalculate_ip_checksum ip_layer size =
-      Wire_structs.Ipv4_wire.set_ipv4_csum ip_layer 0;
-      let just_ipv4 = Cstruct.sub ip_layer 0 size in
-      let new_csum = Tcpip_checksum.ones_complement just_ipv4 in
-      Wire_structs.Ipv4_wire.set_ipv4_csum ip_layer new_csum
-    in
     match Nat_decompose.layers frame with
     | None -> Lwt.return Untranslated (* un-NATtable packet; drop it like it's hot *)
     | Some (frame, ip_packet, higherproto_packet, _payload) ->
@@ -101,11 +54,11 @@ end = struct
           | Some (_expiry, ((V4 new_src, new_sport), (V4 new_dst, new_dport))) ->
             (* TODO: we should probably refuse to pass TTL = 0 and instead send an
                ICMP message back to the sender *)
-            rewrite_ip false ip_packet direction (V4 new_src, V4 new_dst);
-            rewrite_port higherproto_packet direction (new_sport, new_dport);
+            let open Nat_decompose in
+            rewrite_ip false ip_packet (V4 new_src, V4 new_dst);
+            rewrite_port higherproto_packet (new_sport, new_dport);
             decrement_ttl ip_packet;
-            recalculate_ip_checksum ip_packet
-              ((Cstruct.len ip_packet) - (Cstruct.len higherproto_packet));
+            recalculate_ip_checksum ip_packet higherproto_packet;
             Lwt.return Translated
 
           (* TODO: 4-to-6 logic *)
