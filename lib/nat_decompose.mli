b0d92d9dbe389951ec5e7bfa947c14c6aae826bc
Thomas Leonard <talex5@gmail.com>
2017-03-10 11:28:53 +0000

    Remove Nat_decompose internal module
    The only thing it contained were rewrite functions, so moved them to
    Nat_rewrite.

--- lib/nat_decompose.mli
+++
@@ -1,5 +0,0 @@
-(* TODO: in error cases, rewrite_packet should return an ICMP reject message to send back to the packet originator, rather than just a string error *)
-val rewrite_packet :
-  Nat_packet.t ->
-  src:(Ipaddr.V4.t * Mirage_nat.port) -> dst:(Ipaddr.V4.t * Mirage_nat.port) ->
-  (Nat_packet.t, [> `TTL_exceeded | `Untranslated]) Result.result
--- lib/nat_rewrite.ml
+++ lib/nat_rewrite.ml
@@ -3,6 +3,37 @@ open Mirage_nat
 
 open Lwt.Infix
 
+let rewrite_icmp ~src_port icmp =
+  match icmp.Icmpv4_packet.subheader with
+  | Icmpv4_packet.Id_and_seq (_, seq) ->
+    Ok {icmp with Icmpv4_packet.subheader = Icmpv4_packet.Id_and_seq (src_port, seq)}
+  | _ ->
+    Error `Untranslated
+
+let rewrite_packet packet ~src:(src, src_port) ~dst:(dst,dst_port) =
+  let `IPv4 (ip_header, transport) = packet in
+  match Ipv4_packet.(ip_header.ttl) with
+  | 0 -> Error `TTL_exceeded
+  | n ->
+    let ttl = n - 1 in
+    let new_ip_header = { ip_header with Ipv4_packet.src; dst; ttl} in
+    match transport with
+    | `ICMP (icmp_header, payload) ->
+      begin match rewrite_icmp ~src_port icmp_header with
+        | Error _ as e -> e
+        | Ok new_icmp ->
+          Logs.debug (fun f -> f "ICMP header rewritten to: %a" Icmpv4_packet.pp new_icmp);
+          Ok (`IPv4 (new_ip_header, `ICMP (new_icmp, payload)))
+      end
+    | `UDP (_udp_header, udp_payload) ->
+      let new_transport_header = { Udp_packet.src_port; dst_port } in
+      Logs.debug (fun f -> f "UDP header rewritten to: %a" Udp_packet.pp new_transport_header);
+      Ok (`IPv4 (new_ip_header, `UDP (new_transport_header, udp_payload)))
+    | `TCP (tcp_header, tcp_payload) ->
+      let new_transport_header = { tcp_header with Tcp.Tcp_packet.src_port; dst_port } in
+      Logs.debug (fun f -> f "TCP header rewritten to: %a" Tcp.Tcp_packet.pp new_transport_header);
+      Ok (`IPv4 (new_ip_header, `TCP (new_transport_header, tcp_payload)))
+
 module Make(Nat_table : Mirage_nat.Lookup) = struct
   let src = Logs.Src.create "nat-rewrite" ~doc:"Mirage NAT packet rewriter"
   module Log = (val Logs.src_log src : Logs.LOG)
@@ -31,7 +62,7 @@ module Make(Nat_table : Mirage_nat.Lookup) = struct
       | Some (_expiry, ((V4 new_src, new_sport), (V4 new_dst, new_dport))) ->
         (* TODO: we should probably refuse to pass TTL = 0 and instead send an
             ICMP message back to the sender *)
-        begin match Nat_decompose.rewrite_packet packet ~src:(new_src, new_sport) ~dst:(new_dst, new_dport) with
+        begin match rewrite_packet packet ~src:(new_src, new_sport) ~dst:(new_dst, new_dport) with
         | Ok packet -> Lwt.return (Ok packet)
         | Error e ->
           Log.warn (fun f -> f "Translating a packet failed: %a; packet content: %a"
